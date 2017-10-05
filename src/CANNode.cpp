4598ea7efc540f6c832841ea7708ec4c7dd819e0
wiebel <git@wiebel.org>
2017-10-05 22:46:32 +0200

    Revert "git test"
    This reverts commit f4ac188a96c213f6665db2e03689f4f45f09be38.

--- src/CANNode.cpp
+++ src/CANNode.cpp
@@ -1,11 +1,10 @@
 #include <Arduino.h>
-#define FASTLED_ALLOW_INTERRUPTS 0
 
 // -------------------------------------------------------------
 // CANNode for Teensy 3.1/3.2
 //
 // by wiebel
-// foo 
+//
 
 #include <Metro.h>
 #include <FlexCAN.h>
@@ -37,7 +36,6 @@
 #define PIO_B 0x40
 
 #define DS2406_BUF_LEN 10
-
 // Initialisation
 // Misc
 #if DEBUG
